59d5dfcf2c3d65763057a97e4b36d120af8e6aaf
Deydarhdon <Deydarhdon@gmail.com>
2022-09-15 18:34:59 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,20 +2,25 @@
 
 /**
  * more_numbers - prints 10 times the numbers, from 0 to 14
- * Return: Always 0 (Success)
  */
 void more_numbers(void)
 {
 	int i, int j;
 
-	for (i = 0; i < 10; i++)
+	i = 0;
+
+	while (i < 10)
 	{
 		for (j = 0; j < 15; j++)
 		{
 			if (j >= 10)
-				_putchar(j / 10 + '0');
-			_putchar(j % 10 + '0');
+			{
+				_putchar((j / 10) + 48);
+			}
+			_putchar((j % 10) + 48);
 		}
 		_putchar('\n');
+
+		i++;
 	}
 }
